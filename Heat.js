39ddb7a675e3844b2cf1c4e8d21d21e68b622e10
JessThottan <jess.thottan@gmail.com>
2020-01-15 12:00:29 +0530

    check commit

---
+++ Heat.js
@@ -0,0 +1,10 @@
+let fahrenheit = 59
+// fahrenheit to celsius  formula
+//  (fahrenheit− 32) × 5/9
+let celsius = (fahrenheit - 32) * 5/9
+console.log ("fahrenheit = " +fahrenheit +" Degree")
+console.log (celsius +" Degree Celsius")
+
+let kelvin = (fahrenheit - 32) * 5/9 + 273.15
+console.log(kelvin + " K")
+console.log("First commit in source")
